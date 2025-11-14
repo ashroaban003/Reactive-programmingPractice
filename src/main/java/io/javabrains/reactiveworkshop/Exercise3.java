b08fe928881a5c386569e64c0a9d1a4c48d18655
ashroaban003 <ratnakumar48070@gmail.com>
2025-11-14 18:01:53 +0530

    Exercise3

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,9 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Exercise3 {
 
@@ -11,7 +14,8 @@ public class Exercise3 {
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
         // TODO: Write code here
-
+        List<Integer> l = ReactiveSources.intNumbersFlux().toStream().toList();
+        System.out.println(l);
 
         System.out.println("Press a key to end");
         System.in.read();
