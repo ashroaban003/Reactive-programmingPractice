158aa2d66c4e181aee60a6cb6012fb20b8bbf578
ashroaban003 <ratnakumar48070@gmail.com>
2025-11-15 18:07:46 +0530

    exercise4

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.stream.Collector;
 
 public class Exercise4 {
 
@@ -10,9 +11,11 @@ public class Exercise4 {
 
         // Print the value from intNumberMono when it emits
         // TODO: Write code here
+        ReactiveSources.intNumberMono().subscribe(System.out::println);
 
         // Get the value from the Mono into an integer variable
         // TODO: Write code here
+        ReactiveSources.intNumberMono().flux().toStream().findAny().get();
 
         System.out.println("Press a key to end");
         System.in.read();
